3dc4d863cadfafce24921196fb9005218517552f
Etukeni Ndecha <etukeni.e.ndecha@lynkco.com>
2023-11-12 23:24:44 +0000

    Cors

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,8 @@ import {AuthProvider, Options} from "react-admin";
 import { fetchUtils, DataProvider } from 'ra-core';
 import { stringify } from 'query-string';
 
-const host = "http://ec2-52-56-116-147.eu-west-2.compute.amazonaws.com:4545/"
-//const host = "https://web-production-dea6.up.railway.app/http://ec2-52-56-116-147.eu-west-2.compute.amazonaws.com:4545/"
+//const host = "http://ec2-52-56-116-147.eu-west-2.compute.amazonaws.com:4545/"
+const host = "https://web-production-dea6.up.railway.app/http://ec2-52-56-116-147.eu-west-2.compute.amazonaws.com:4545/"
 
 const apiUrl = host + 'allsaints/api/v1';
 const httpClient = fetchUtils.fetchJson;
@@ -148,7 +148,6 @@ export const authProvider: AuthProvider = {
         const request = new Request(url, {
             method: 'POST',
             body: JSON.stringify({phoneNumber, otp}),
-            mode: "cors",
             headers: new Headers({'Content-Type': 'application/json'},)
         });
 
