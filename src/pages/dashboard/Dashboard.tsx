c900806617e9b9af38602d2e72fb0e456cd63416
Etukeni Ndecha <etukeni.e.ndecha@lynkco.com>
2023-11-13 02:36:03 +0000

    Ros

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -5,16 +5,16 @@ import MoneyCard from './MoneyCard';
 
 export const Dashboard = () => (
     <Grid container spacing={2} mt={1}>
-        <Grid item xs={12} md={2}>
+        <Grid item xs={12} md={3}>
             <NumberCard value={"300"} title={'pos.dashboard.venue_capacity'}/>
         </Grid>
-        <Grid item xs={12} md={2}>
+        <Grid item xs={12} md={3}>
             <NumberCard value={"114"} title={'pos.dashboard.tickets_sold'}/>
         </Grid>
-        <Grid item xs={12} md={2}>
+        <Grid item xs={12} md={3}>
             <NumberCard title={'pos.dashboard.tickets_remaining'} value={"186"}/>
         </Grid>
-        <Grid item xs={12} md={2}>
+        <Grid item xs={12} md={3}>
             <MoneyCard title={'pos.dashboard.money_collected'} value={"TBC"}/>
         </Grid>
         <Grid item xs={12} />
