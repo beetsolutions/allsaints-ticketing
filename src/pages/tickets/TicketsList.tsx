3eb670dd208391d45e9208436abcd62abe3b07a3
Etukeni Ndecha <etukeni.e.ndecha@lynkco.com>
2023-11-22 18:49:09 +0000

    Fixup

--- src/pages/tickets/TicketsList.tsx
+++ src/pages/tickets/TicketsList.tsx
@@ -13,9 +13,6 @@ const UserBulkActionButtons = (props : any) => (
     <BulkDeleteWithConfirmButton {...props} />
 );
 
-const postRowStyle = (record: any, index: number) => ({
-    backgroundColor: record.paymentStatus!== 'SOLD' ? '#efe' : '#e2e2e2',
-});
 
 const TicketsList = () => {
     return (
@@ -23,12 +20,14 @@ const TicketsList = () => {
             sort={{ field: 'usageStatus', order: 'ASC' }}
         >
             {useMediaQuery((theme: Theme) => theme.breakpoints.down('md')) ? (
-                <SimpleList
-                    secondaryText={<TextField source="id" />}
-                    primaryText={record => `${record.views} views`}
-                    linkType={record => record.paymentStatus!== 'SOLD' ? "edit" : "show"} 
-                    rowStyle={postRowStyle}
-                />
+                <Datagrid
+            >
+                <TextField source="id" />
+                <TextField source="paymentStatus" />
+                <TextField source="usageStatus" />
+                <TextField source="type" />
+                <TextField source="price" />
+            </Datagrid>
             ) : (
                 <Datagrid
                     expand={<TicketsEditEmbedded />}
