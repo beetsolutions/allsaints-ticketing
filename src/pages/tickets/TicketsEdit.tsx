b5656aa51724721d0a95e26d83e5f4fb31f396a8
Etukeni Ndecha <etukeni.e.ndecha@lynkco.com>
2023-11-23 20:01:53 +0000

    Test

--- src/pages/tickets/TicketsEdit.tsx
+++ src/pages/tickets/TicketsEdit.tsx
@@ -1,27 +1,34 @@
-import {Edit, required, SimpleForm, TextInput} from 'react-admin';
+import {Edit, required, SimpleForm, TextInput, useRecordContext} from 'react-admin';
 import {RichTextInput, RichTextInputToolbar} from 'ra-input-rich-text';
 
-const TicketsEdit = () => (
-    <Edit>
+const TicketsEdit = () => {
+
+    const record = useRecordContext();
+
+    const disabledValue = record.paymentStatus === 'SOLD' ? true : false
+
+    return (
+        <Edit>
         <SimpleForm>
             <TextInput name="id" disabled source="id"/>
-            <TextInput name="paymentStatus" source="paymentStatus" validate={[required()]} disabled />
-            <TextInput name="usageStatus" source="usageStatus" validate={[required()]} disabled />
-            <TextInput name="price" source="price" validate={[required()]} disabled/>
-            <TextInput name="type" source="type" validate={[required()]} disabled />
+            <TextInput name="paymentStatus" source="paymentStatus" validate={[required()]} disabled={disabledValue} />
+            <TextInput name="usageStatus" source="usageStatus" validate={[required()]} disabled={disabledValue} />
+            <TextInput name="price" source="price" validate={[required()]} disabled={disabledValue}/>
+            <TextInput name="type" source="type" validate={[required()]} disabled={disabledValue} />
             <TextInput type={"hidden"} name="updatedByMemberId" source="updatedByMemberId"
                        validate={[required()]}
                        defaultValue={localStorage.getItem('id')}
                        value={localStorage.getItem('id')}
-                       disabled
+                       disabled={disabledValue}
             />
             <RichTextInput
                 name="notes"
                 source="notes" toolbar={<RichTextInputToolbar size="large"/>}
-                disabled
+                disabled={disabledValue}
             />
         </SimpleForm>
     </Edit>
-);
+    );
+}
 
 export default TicketsEdit;
--- src/pages/tickets/TicketsEditEmbedded.tsx
+++ src/pages/tickets/TicketsEditEmbedded.tsx
@@ -1,29 +1,35 @@
 import PropTypes from 'prop-types';
-import {Edit, Identifier, SimpleForm, TextInput, required} from 'react-admin';
+import {Edit, Identifier, SimpleForm, TextInput, required, useRecordContext} from 'react-admin';
 import {RichTextInput, RichTextInputToolbar} from "ra-input-rich-text";
 
-const TicketsEditEmbedded = ({ id }: { id?: Identifier }) => (
-    <Edit title=" " id={id}>
-        <SimpleForm>
-            <TextInput name="paymentStatus" source="paymentStatus" validate={[required()]} disabled/>
-            <TextInput name="usageStatus" source="usageStatus" validate={[required()]} disabled />
-            <TextInput name="price" source="price" validate={[required()]} disabled/>
-            <TextInput name="type" source="type" validate={[required()]} disabled/>
-            <TextInput type={"hidden"} name="updatedByMemberId"
-                       source="updatedByMemberId"
-                       validate={[required()]}
-                       defaultValue={localStorage.getItem('id')}
-                       value={localStorage.getItem('id')}
-                       disabled
-            />
-            <RichTextInput
-                name="notes"
-                source="notes" toolbar={<RichTextInputToolbar size="large"/>}
-                disabled
-            />
-        </SimpleForm>
-    </Edit>
-);
+const TicketsEditEmbedded = ({ id }: { id?: Identifier }) => {
+
+    const record = useRecordContext();
+
+    const disabledValue = record.paymentStatus === 'SOLD' ? true : false
+    return (
+        <Edit title=" " id={id}>
+            <SimpleForm>
+                <TextInput name="paymentStatus" source="paymentStatus" validate={[required()]} disabled={disabledValue}/>
+                <TextInput name="usageStatus" source="usageStatus" validate={[required()]} disabled={disabledValue} />
+                <TextInput name="price" source="price" validate={[required()]} disabled={disabledValue}/>
+                <TextInput name="type" source="type" validate={[required()]} disabled={disabledValue}/>
+                <TextInput type={"hidden"} name="updatedByMemberId"
+                           source="updatedByMemberId"
+                           validate={[required()]}
+                           defaultValue={localStorage.getItem('id')}
+                           value={localStorage.getItem('id')}
+                           disabled={disabledValue}
+                />
+                <RichTextInput
+                    name="notes"
+                    source="notes" toolbar={<RichTextInputToolbar size="large"/>}
+                    disabled={disabledValue}
+                />
+            </SimpleForm>
+        </Edit>
+    );
+}
 
 TicketsEditEmbedded.propTypes = {
     record: PropTypes.object,
